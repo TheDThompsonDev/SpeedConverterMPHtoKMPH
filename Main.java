47115d6a4b89028ce35e09bab28c2789d84829d6
Danny Thompson <dannythompson901@gmail.com>
2019-05-22 06:05:47 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean value = BarkingDog.shouldWakeUp(true, 10);
+        System.out.println(value);
+    }
+}
+
---
+++ SpeedConverter.java
@@ -0,0 +1,20 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour <0) {
+            return -1;
+        }
+        long milesPerHour = Math.round(kilometersPerHour / 1.609);
+        return milesPerHour;
+
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            System.out.println("Invalid value");
+        }
+        else{
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + "km/h= " + " mi/h");
+        }
+    }
+}
